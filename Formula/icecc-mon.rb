0a423a71026d9035838842caee82818f43e23cc8
Tim Blechmann <tim@klingt.org>
2026-05-24 19:07:13 +0800

    Bump icecc-mon to 0.1.1

--- Formula/icecc-mon.rb
+++ Formula/icecc-mon.rb
@@ -1,8 +1,8 @@
 class IceccMon < Formula
   desc "A lightweight TUI monitor for icecc distributed compilation"
   homepage "https://github.com/timblechmann/icecc-mon-rs"
-  url "https://github.com/timblechmann/icecc-mon-rs/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "9c1d9f30fc48357f2a343b72678d73d79a048f24392017a177d9aca196d2c514"
+  url "https://github.com/timblechmann/icecc-mon-rs/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "0c0c76e0260b19d3993742cb7f64ad89ba5a644994509152b954199f7d99db1c"
   license "GPL-2.0-only"
 
   depends_on "rust" => :build
