48c62146c19746155df73ab3547aba31c2f2a313
Tim Blechmann <tim@klingt.org>
2026-05-25 18:10:29 +0800

    fix checksum

--- Formula/icecc-mon.rb
+++ Formula/icecc-mon.rb
@@ -2,7 +2,7 @@ class IceccMon < Formula
   desc "A lightweight TUI monitor for icecc distributed compilation"
   homepage "https://github.com/timblechmann/icecc-mon-rs"
   url "https://github.com/timblechmann/icecc-mon-rs/archive/refs/tags/v0.1.6.tar.gz"
-  sha256 "8c7dc11f4fe472ddc4310d0102462430ddcd2c24772ad60bc687eef56b2f92d2"
+  sha256 "207c9d4e5ef5bb8b418a3abd06bb3992347af4ae61595da89a2b219702ed2562"
   license "GPL-2.0-only"
 
   depends_on "rust" => :build
